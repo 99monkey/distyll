5b3a3f2ae6c962a91a6d3b6ce28bf732470c0f3d
Mason F. Matthews <mason.f.matthews@gmail.com>
2014-05-21 20:21:43 +0000

    Add model tree builders. Exclude :through associations.

--- lib/distyll.rb
+++ lib/distyll.rb
@@ -1,37 +1,64 @@
 class Distyll
 
-  attr_accessor :base_model, :created_since
+  attr_reader :base_models, :model_forest, :created_since
 
-  def initialize(bm, cs)
-    @base_model = bm.constantize #TODO: Allow more than one base model
+  def initialize(bms, cs)
     @created_since = cs.to_date
+    @base_models = bms.map &:constantize
+    @model_forest = Distyll.build_model_forest(base_models)
   end
 
   def run
-    records = base_model.where("created_at >= ?", created_since).order(:id)
-    run_on_associations(base_model, records)
-    copy_records(base_model, records)
+    base_models.each do |base_model|
+      records = base_model.where("created_at >= ?", created_since).order(:id).load
+      run_on_associations(records)
+      copy_records(records)
+    end
   end
+
+  def self.build_model_forest(model_array)
+    tree = {}
+    model_array.each { |m| tree[m] = build_model_tree(m) }
+    tree
+  end
+
+  def self.build_model_tree(model)
+    branch = {}
+    model.reflect_on_all_associations.each do |association|
+      if association.belongs_to? && association.through_reflection.nil?
+        branch[association.klass] = build_model_tree(association.klass)
+      end
+    end
+    branch
+  end
+
 
   private
 
   #TODO: keep track of which models have already been traversed, add to those.
-  #TODO: are foreign keys going to be preserved correctly?
-  def run_on_associations(model, records)
+  #TODO: "oh, crap" on self-referential joins
+  def run_on_associations(records)
+    return if records.blank?
+
+    model = records.first.class
     puts "Starting Associations for #{model.to_s}"
-    model.reflect_on_all_associations.each do |a|
-      if a.belongs_to? #TODO: and not a through
-        associated_records = get_associated_records(records, a)
-        run_on_associations(a.klass, associated_records)
-        copy_records(a.klass, associated_records)
+
+    model.reflect_on_all_associations.each do |association|
+      if association.belongs_to? && association.through_reflection.nil?
+        associated_records = get_associated_records(records, association)
+        run_on_associations(associated_records)
+        copy_records(associated_records)
       end
     end
   end
 
-  def copy_records(model, records)
+  def copy_records(records)
+    return if records.blank?
+
+    model = records.first.class
     puts "Copying Records for #{model.to_s}"
-    records = records.to_a
-    model.establish_connection("distyll") #TODO: get model off of records rather than passing it in?
+
+    model.establish_connection("distyll")
     records.each do |record|
       model.create!(record.attributes)
     end
@@ -40,9 +67,10 @@ class Distyll
 
   def get_associated_records(records, association)
     puts "Getting Associated #{association.name.to_s.titleize.pluralize}"
+
     associated_records = []
     records.each do |r|
-      associated_records << r.send(association.name) #TODO: only works for belongs_to.  Need to consider +=
+      associated_records << r.send(association.name) #TODO: only works for belongs_to.  Need to consider += for other association types
     end
     associated_records.compact.uniq
   end
